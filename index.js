8e6de3c0bed9e8fdffcdd015a23b890a28c197b2
singhnatwar25 <singhnatwar20121@gmail.com>
2024-05-22 04:16:21 +0530

    s

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
+const app = express();
 const favicon = require('serve-favicon');
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
 
-const app = express();
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
