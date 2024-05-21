2dcc8c742a770d2a9e3af58cb1e0275b5850f761
singhnatwar25 <singhnatwar20121@gmail.com>
2024-05-22 03:51:11 +0530

    s

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
-const router = express.Router(); // Define and initialize router
 
-app.use(express.static('public')); // Serve static files from 'public' directory
-
-// Define routes using the router object
-router.get('/', (req, res) => {
-    res.sendFile('index.html', { root: './public' });
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
 });
 
-router.get('/hello', (req, res) => {
-    res.sendFile('hello.html', { root: './public' });
-});
-
-// Use the router middleware in your Express app
-app.use(router);
-
-// Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
 });
 
 // Export the Express API
